ca65312ac69ed18473bbcf38e457248557f7c298
rajdesai4580-cloud <rajdesai4580@gmail.com>
2026-04-16 15:42:14 +0530

    Revert "Initial commit"
    This reverts commit 9d81b9f553ba8ca2315e911751b9213876d31ad1.

--- server/src/queues/index.ts
+++
@@ -1,5 +0,0 @@
-import { Queue } from 'bullmq';
-import { connection } from '../config/redis';
-
-export const importQueue = new Queue('ImportQueue', { connection });
-export const processQueue = new Queue('ProcessQueue', { connection });
--- server/src/routes/importRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getImportLogs, triggerImport } from '../controllers/importController';
-
-const router = Router();
-
-router.get('/logs', getImportLogs);
-router.post('/trigger', triggerImport);
-
-export default router;
